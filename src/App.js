04b2688137d339bf2e4efaab5fa39e6e8faf8bdf
marcinoles87 <marcinoles87@gmail.com>
2023-09-21 05:48:01 +0200

    start with project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Budget from './components/Budget';
 
 function App() {
   return (
     <div className="App">
-    
+      <h1 className='mt-3'>Budget Planner</h1>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <Budget></Budget>
+        </div>
+      </div>
     </div>
   );
 }

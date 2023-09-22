25ddcc221aebde7af9ecb962f8741b0cd2db0472
marcinoles87 <marcinoles87@gmail.com>
2023-09-22 05:50:10 +0200

    add components

--- src/components/ExpensItem.js
+++ src/components/ExpensItem.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function ExpensItem({id , name , cost}) {
   return (
-    <div key={id}>{name} <span>{cost}</span></div>
+    <tr key={id}>
+    <th scope='row' >
+        <td>{name}</td>
+        <td>{cost}</td>
+
+        </th>
+        </tr>
   )
 }
 

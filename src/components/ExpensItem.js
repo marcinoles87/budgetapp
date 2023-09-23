5bb8fc5986e0f66d0943345f88e2b6ef6cd07936
marcinoles87 <marcinoles87@gmail.com>
2023-09-23 09:54:22 +0200

    add components

--- src/components/ExpensItem.js
+++ src/components/ExpensItem.js
@@ -3,7 +3,9 @@ import { TiDelete} from 'react-icons/ti'
 
 function ExpensItem({id , name , cost}) {
   return (
-    <li className='list-group-item'>{name} <span className='badge  bg-primary'> {cost} pln</span>
+    <li className='list-group-item'>
+        {name} <span className='badge  bg-primary'> {cost} pln</span>
+      <TiDelete></TiDelete>
       </li>
   )
 }

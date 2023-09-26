9cfecab078cc0a9687f0dbaa8e23f53c9166464b
marcinoles87 <marcinoles87@gmail.com>
2023-09-26 16:11:55 +0200

    add function cout cost

--- src/components/BuyItem.js
+++ src/components/BuyItem.js
@@ -1,42 +1,37 @@
 import React, { useState } from 'react'
 
-function BuyItem({costItemBuy}) {
-  const [item , setItem] = useState({})
-
-const message = 'from child to parent'
+function BuyItem() {
+  const [item , setItem] = useState(['www'])
   
-  const handleOnChange = (e) => {
-    const target = e.target.value
-    const target2 = e.target.value
-    console.log(target)
-    setItem({
-      name : target ,
-      cost : target2
-    })
+  const handleOnChange = (e , i) => {
+   const arrayOne = [...item]
+   arrayOne[i] = e.target.value
+   setItem(arrayOne)
   }
 
-  const handleOnChange2 = (e) => {
-    const target = e.target.value
-    console.log(target)
-    setItem({
-      cost : target
-    })
-  }
+  console.log(item)
+  
 
   const handleOnClick = () => {
-    console.log(`dodano produkt ${item.name} `)
-    console.log(`dodano produkt ${item.cost} `)
-    costItemBuy(message)
+    const abc = [...item, []]
+    setItem(abc)
   }
   
   return (
-    <div className="input-group mb-3">
-  <input onChange={handleOnChange}  type="text" className="form-control" placeholder="item buy" aria-label="" aria-describedby="basic-addon1"></input>
-  <input onChange={handleOnChange2}   type="number" className="form-control" placeholder="item cost/pln" aria-label="" aria-describedby="basic-addon1"></input>
+    <>
+    {item.map( (data,i) => {
+      return(
+      <div className="input-group mb-3">
+  <input value={data} onChange={e => handleOnChange(e,i)}  type="text" className="form-control" placeholder="item buy" aria-label="" aria-describedby="basic-addon1"></input>
+  {/* <input onChange={handleOnChange}   type="number" className="form-control" placeholder="item cost/pln" aria-label="" aria-describedby="basic-addon1"></input> */}
   <div className="input-group-prepend">
     <button  onClick={handleOnClick} className="btn btn-outline-secondary" type="button">Add Expens</button>
   </div>
-</div>
+</div>)
+    })}
+
+    </>
+    
   )
 }
 
